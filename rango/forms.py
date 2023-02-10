50942a43b7b4378039543c4b8e0dbceec49f556f
Mohammad Torki <2727009t@student.gla.ac.uk>
2023-02-10 12:52:53 +0500

    29th commit

--- rango/forms.py
+++ rango/forms.py
@@ -32,3 +32,13 @@ class PageForm(forms.ModelForm):
 		# we can either exclude the category field from the form,
 		exclude = ('category',)
 		# or specify the fields to include
+
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+		# If url is not empty and doesn't start with 'http://',
+		# then prepend 'http://'.
+		if url and not url.startswith('http://'):
+			url = f'http://{url}'
+			cleaned_data['url'] = url
+		return cleaned_data
