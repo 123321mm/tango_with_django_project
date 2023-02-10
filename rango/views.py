844a3d8a2b5c22003cb3297d2d6c04256e74ae09
Mohammad Torki <2727009t@student.gla.ac.uk>
2023-02-10 18:55:44 +0500

    37th commit

--- rango/views.py
+++ rango/views.py
@@ -10,8 +10,27 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
 
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		# Set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+
+	request.session['visits'] = visits
 
 def show_category(request, category_name_slug):
 	# Create a context dictionary which we can pass
@@ -46,15 +65,17 @@ def index(request):
 	# Retrieve the top 5 only -- or all if less than 5.
 	# Place the list in our context_dict dictionary (with our boldmessage!)
 	# that will be passed to the template engine.
-	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
 	context_dict = {}
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
 	pages_list = Page.objects.order_by('-views')[:5]
 	context_dict['pages'] = pages_list
-	# Render the response and send it back!
+	
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 	return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
 	if request.session.test_cookie_worked():
